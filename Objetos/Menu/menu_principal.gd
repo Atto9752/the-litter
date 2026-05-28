3ccc3d90ab0c297babca766dc289ada4078c3219
Atto9752 <anto.faundes@duocuc.cl>
2026-05-28 03:32:59 -0400

    Integracion con Dialogic
    Se integro la batalla por turnos con Dialogic.

--- Objetos/Menu/menu_principal.gd
+++ Objetos/Menu/menu_principal.gd
@@ -5,8 +5,8 @@ func _ready() -> void:
 	$Button.grab_focus()
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Objetos/Mundo/mundo_demo.tscn") 
+	get_tree().change_scene_to_file("res://Dialogic/Escenas/escena_intro.tscn") 
 
-
+# salir del juego
 func _on_button_3_pressed() -> void:
 	get_tree().quit() 
